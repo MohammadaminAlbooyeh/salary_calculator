fe2e7faf8c875133b0c8bd350805968a45c2437a
Amin Albooyeh <amin.albooyeh@gmail.com>
2026-04-01 18:36:10 +0200

    add port variable into the main.py

--- backend/main.py
+++ backend/main.py
@@ -27,4 +27,6 @@ def calculate_salary():
     return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5051)
+    import os
+    port = int(os.environ.get('PORT', 5051))
+    app.run(host='0.0.0.0', port=port)
